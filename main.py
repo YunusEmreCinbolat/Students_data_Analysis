91c8e6044804e3e4032bc93d0cdefe728c00d3c9
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-29 19:30:56 +0300

    translated turkish variable names translated to english language until line 75

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 pd.set_option('display.max_columns',None)
 pd.set_option('display.width',500)
-#farklı dataset ile birleştir
-#kaynak
+
+#combine with different dataset
+
+#https://www.kaggle.com/datasets/joebeachcapital/students-performance
 df=pd.read_csv("DATA.csv")
 print(df.head(10000000))
 
@@ -13,67 +16,59 @@ print(df.head(10000000))
 #print(df.info())
 #print(df.isnull().sum())
 
-
-
-plt.figure(figsize=(12,6))
-
 #future engineering alanını dahil et
-#ingilizce isimlendirme
-yas_grubu=["22-25", "18-21", "26-"]
-yas_araliklari = df['1'].value_counts()
+age_group=["22-25", "18-21", "26-"]
+age_intervals = df['1'].value_counts()
 
 # almak istenen sonuca göre detaylı görselleştirme araçlarını  incele (chart tipi)
 
-
-
-# Yas aralıklarına göre kişi sayısı Sonuçları görüntüleyin
-print(yas_araliklari)
+# Number of people by age range View results
+print(age_intervals)
 plt.figure(figsize=(12,6))
-sns.barplot(x=yas_grubu,y=yas_araliklari,data=yas_araliklari,palette='viridis')
+sns.barplot(x=age_group, y=age_intervals, data=age_intervals, palette='viridis')
 plt.title("Yas gruplarına göre öğrenci sayisi")
 plt.xlabel("Yas aralıkları")
 plt.ylabel("Kisi sayısı")
 plt.show()
 
-# Cinsiyete göre kişi sayısı Sonuçları görüntüleyin
-
-cinsiyete_gore_dagilim=df["2"].value_counts()
+# Number of people by gender View results
+gender_gore_distribution=df["2"].value_counts()
 plt.figure(figsize=(12,6))
-sns.barplot(x=["Erkek","Kadın"],y=cinsiyete_gore_dagilim,data=cinsiyete_gore_dagilim,palette='viridis')
+sns.barplot(x=["Erkek","Kadın"], y=gender_gore_distribution, data=gender_gore_distribution, palette='viridis')
 plt.title("Cinsiyete göre dağılım")
 plt.xlabel("Cinsiyet")
 plt.ylabel("Kişi sayısı")
 
 plt.show()
 
-# Her Yas grubuna göre cinsiyet sayısı Sonuçları görüntüleyin
-
-for yas_grubu in yas_araliklari.index:
-    yasli_veri = df[df['1'] == yas_grubu]
-    cinsiyet_dagilimi = yasli_veri["2"].value_counts()
-    print(f'{yas_grubu} Yaş Grubu Cinsiyet Dağılımı:')
-    print(cinsiyet_dagilimi)
+# Number of sexes by each age group View results
+for age_group in age_intervals.index:
+    mourning_data = df[df['1'] == age_group]
+    gender_distribution = mourning_data["2"].value_counts()
+    print(f'{age_group} Yaş Grubu Cinsiyet Dağılımı:')
+    print(gender_distribution)
     print("\n")
     plt.figure(figsize=(12, 6))
-    sns.barplot(x=cinsiyet_dagilimi, y=["Erkek","Kadın"], palette="viridis")
-    plt.title(f'{yas_grubu.numerator} Yaş Grubu Cinsiyet Dağılımı')
+    sns.barplot(x=gender_distribution, y=["Erkek", "Kadın"], palette="viridis")
+    plt.title(f'{age_group.numerator} Yaş Grubu Cinsiyet Dağılımı')
     plt.xlabel("Cinsiyet")
     plt.ylabel("Kişi Sayısı")
     plt.show()
 
-#Öğrencilerin ne kadarı hangi tür liseden mezun
-lise_grup_ad=["Devlet","Özel","Diğer"]
-lise_grup=df['3'].value_counts()
-print(lise_grup)
+#How many students graduated from which type of high school
+high_school_group_name=["Devlet", "Özel", "Diğer"]
+high_school_group=df['3'].value_counts()
+print(high_school_group)
 plt.figure(figsize=(12,6))
-sns.barplot(x=lise_grup_ad,y=lise_grup,data=lise_grup,palette="viridis")
+sns.barplot(x=high_school_group_name, y=high_school_group, data=high_school_group, palette="viridis")
 plt.xlabel("Lise türü")
 plt.ylabel("Kişi sayısı")
 plt.show()
 
-burslu_kisi=df["4"].value_counts()
-burs_miktar=["50","75","100","25","0"]
-sns.barplot(x=burs_miktar,y=burslu_kisi,data=burslu_kisi,palette='viridis')
+#Number of students according to scholarship amounts
+scholarship_kisi=df["4"].value_counts()
+scholarship_amount=["50", "75", "100", "25", "0"]
+sns.barplot(x=scholarship_amount, y=scholarship_kisi, data=scholarship_kisi, palette='viridis')
 plt.xlabel("Burs miktarları")
 plt.ylabel("Kişi sayısı")
 plt.show()
